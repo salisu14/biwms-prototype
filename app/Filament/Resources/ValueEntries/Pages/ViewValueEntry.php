2772742058f906abd0de9efd6355e7706666cecf
Muhammad Salisu Ali <salisu14@yahoo.com>
2026-06-21 19:08:11 +0100

    feat: Update Value Entries to remove unnecessary actions - disable create and edit buttons

--- app/Filament/Resources/ValueEntries/Pages/ViewValueEntry.php
+++ app/Filament/Resources/ValueEntries/Pages/ViewValueEntry.php
@@ -13,7 +13,7 @@ class ViewValueEntry extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            EditAction::make(),
+//            EditAction::make(),
         ];
     }
 }
